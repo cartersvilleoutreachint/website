ace33146228a67d006143b8c482182c290b2f2c0
William Bojczuk <williambojczuk@gmail.com>
2023-08-30 18:11:15 -0400

    added some more dashboard team stuffs

---
+++ src/app/(dashboard)/dashboard/components/(pages)/TeamEditor/AddTeamMemberForm/AddTeamMemberForm.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { useState, useRef } from "react";
+import styles from "./addteammemberform.module.css"
+import styles2 from "../teameditor.module.css"
+import Image from "next/image";
+
+export default function AddTeamMemberForm() {
+
+  const [currentImg, setCurrentImg] = useState("/img/blank-profile.jpg");
+  const [formIsActive, setFormIsActive] = useState(false)
+  const shaderRef: any = useRef();
+  const wrapperRef: any = useRef();
+
+  function formSubmitHandler(evt: any){
+    evt.preventDefault();
+  }
+
+ 
+
+  function closeForm(){
+    shaderRef.current.ontransitionend = ()=>{
+      shaderRef.current.style.pointerEvents = "none"
+      wrapperRef.current.style.pointerEvents = "none"
+
+      shaderRef.current.ontransitionend = undefined;
+    }
+    shaderRef.current.style.opacity = "0";
+    wrapperRef.current.style.opacity = "0";
+    wrapperRef.current.style.transform = "translateY(-20px)";
+  }
+
+  return (
+    <aside id="formWrapper" ref={wrapperRef} className={`center ${styles.formWrapper}`}>
+      <form className={styles.form} onSubmit={formSubmitHandler}>
+      <div className={styles.profileWrapper}>
+            <Image className={styles.profileImg} height={100} width={100} src={currentImg} alt="Profile Image" />
+            <div className={styles.profileTitle}>Profile Photo</div>
+            <label htmlFor="photoInput" className={styles2.photoInput}>Choose File</label>
+            <input type="file" name="photoInput" id="photoInput" style={{display: "none"}}  />
+        </div>
+
+        <div className={styles.inputWrapper}>
+            <input required placeholder="Name" type="text" name="nameInput" id="nameInput" className={styles.nameInput} />
+            <input required placeholder="Role" type="text" name="roleInput" id="roleInput" className={styles.roleInput} />
+            <textarea required placeholder="Description" name="descInput" id="descInput" className={styles.descInput}></textarea>
+        </div>
+        <div className="center">
+          <input type="submit" className="main-button" value="Add Member" />
+        </div>
+        <button onClick={closeForm} className={styles.closeForm}>&times;</button>
+      </form>
+      
+      <div
+      id="formShader"
+      className={`shader ${styles.shader}`}
+      ref={shaderRef}
+      ></div>
+    </aside>
+  )
+}
--- src/app/(dashboard)/dashboard/components/(pages)/TeamEditor/TeamEditor.tsx
+++ src/app/(dashboard)/dashboard/components/(pages)/TeamEditor/TeamEditor.tsx
@@ -1,6 +1,9 @@
+"use client"
+
 import styles from "./teameditor.module.css"
 import getTeamMembers from "@/app/(mainsite)/components/Pages/OurTeam/TeamMembers/getTeamMembers"
 import TeamMemberEditor from "./TeamMemberEditor"
+import AddTeamMemberForm from "./AddTeamMemberForm/AddTeamMemberForm"
 
 export default function TeamEditor() {
 
@@ -8,10 +11,19 @@ export default function TeamEditor() {
         return <TeamMemberEditor key={i} {...data} />
     })
 
+    function openForm(){
+      document.getElementById("formShader")!.style.pointerEvents = "all"
+      document.getElementById("formShader")!.style.opacity = "1";
+      document.getElementById("formWrapper")!.style.pointerEvents = "all";
+      document.getElementById("formWrapper")!.style.opacity = "1";
+      document.getElementById("formWrapper")!.style.transform = "translateY(0)";
+    }
+
   return (
     <div className="center">
         <div className={styles.wrapper}>
-            <button className={styles.addButton}>+</button>
+            <AddTeamMemberForm />
+            <button onClick={openForm} className={styles.addButton}>+</button>
             {teamElems}
         </div>
     </div>
