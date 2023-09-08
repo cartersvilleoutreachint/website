ea098bbdf67b0e09a5030512451a1bd3c87c6340
William Bojczuk <williambojczuk@gmail.com>
2023-09-08 08:40:06 -0400

    finalized contact form

--- src/app/(mainsite)/components/Pages/Contact/ContactForm/ContactForm.tsx
+++ src/app/(mainsite)/components/Pages/Contact/ContactForm/ContactForm.tsx
@@ -5,7 +5,7 @@ export default function ContactForm() {
     <section className={styles.contactFormSection}>
         <div className={`center ${styles.contactFormWrapper}`}>
         <h3 className={styles.contactFormTitle}>Contact Form</h3>
-            <form method="POST" action="https://formsubmit.co/cartersvilleoutreach@yahoo.com" className={styles.contactForm}>
+            <form method="POST" action="https://formsubmit.co/7a42c42083428f22fb9adf5938b844de " className={styles.contactForm}>
                 <label htmlFor="nameInput">Name</label>
                 <input required className={styles.nameInput} type="text" name="name" id="nameInput" />
 
@@ -19,7 +19,7 @@ export default function ContactForm() {
                     <input className={styles.submitButton} type="submit" value="Send Message" />
                 </div>
 
-                <input type="hidden" name="_next" value="http://localhost:3000?messagesuccess=true" />
+                <input type="hidden" name="_next" value="https://cartersvilleoutreachinternational/contact?messagesuccess=true" />
                 <input type="hidden" name="_template" value="table" />
             </form>
         </div>
