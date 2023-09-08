142c16b329edf10957185d36632c304db6cf3a28
William Bojczuk <williambojczuk@gmail.com>
2023-09-08 10:21:00 -0400

    fixed loading errors on static pages

--- src/app/(dashboard)/dashboard/(pages)/blog/page.tsx
+++ src/app/(dashboard)/dashboard/(pages)/blog/page.tsx
@@ -1,13 +1,9 @@
-import { Suspense } from "react"
-import Loading from "@/app/(mainsite)/components/Misc/Loading/Loading"
 import DashboardBlogList from "../../components/(pages)/Blog/DashboardBlogList"
 
 export default function DashboardBlogPage(){
     return(
         <>
-        <Suspense fallback={<Loading/>}>
             <DashboardBlogList search="default" />
-        </Suspense>
         </>
     )
 }
--- src/app/(dashboard)/dashboard/(pages)/events/page.tsx
+++ src/app/(dashboard)/dashboard/(pages)/events/page.tsx
@@ -1,13 +1,9 @@
-import { Suspense } from "react"
-import Loading from "@/app/(mainsite)/components/Misc/Loading/Loading"
 import DashboardEvents from "../../components/(pages)/Events/DashboardEvents"
 
 export default function DashboardEventsPage(){
     return(
         <>
-        <Suspense fallback={<Loading/>}>
             <DashboardEvents search="default" />
-        </Suspense>
         </>
     )
 }
