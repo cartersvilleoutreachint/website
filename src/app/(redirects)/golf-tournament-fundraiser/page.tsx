4e66ee3e56ed155c09120822f9e99d150917af25
William Bojczuk <williambojczuk@gmail.com>
2023-09-15 10:09:21 -0400

    Fixed broken womens center QR code

---
+++ src/app/(redirects)/golf-tournament-fundraiser/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { useEffect } from "react"
+export default function GolfRedir() {
+    
+    useEffect(()=>{
+        window.location.href = "https://cartersvilleoutreachwomenscenter.org/golf-tournament-fundraiser"
+    }, [])
+
+    return (
+      <></>
+    )
+  }
