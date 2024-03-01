1cf9a65242c99404a037b669f57a0ab21d5b4070
William Bojczuk <williambojczuk@gmail.com>
2024-03-01 10:12:15 +0200

    updated

--- src/app/(mainsite)/components/Pages/HomePage/ComingSoon/ComingSoon.tsx
+++ src/app/(mainsite)/components/Pages/HomePage/ComingSoon/ComingSoon.tsx
@@ -2,6 +2,6 @@ import styles from "./comingsoon.module.css"
 
 export default function ComingSoon() {
   return (
-    <a href="https://cartersvilleoutreachmenscenter.org" target="_blank" className={styles.comingSoon}>Men's Center, Coming February 2024!</a>
+    <a href="https://cartersvilleoutreachmenscenter.org" target="_blank" className={styles.comingSoon}>Men's Center, Now Open!</a>
   )
 }
